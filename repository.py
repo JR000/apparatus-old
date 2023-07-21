15121fc28678684125ce12c4823d807f7ddb5b07
Ivan Ryzhkov <32067622+JR000@users.noreply.github.com>
2023-07-21 14:14:09 +0300

    Update repository.py

--- repository.py
+++ repository.py
@@ -1,33 +1,16 @@
 import dto
-    
+
 class Repository:
-    def queryBooks(self, query: Logic) -> list[dto.Book]:
+    def query(self, table: type, logic: dto.Logic, populates: dto.DTO = None) -> list[dto.DTO]:
         raise NotImplementedError()
-    def queryTokens(self, query: Logic) -> list[dto.Token]:
-        ...
-    def queryCards(self, query: Logic) -> list[dto.Card]:
-        ...
-    def querypParagraphs(self, query: Logic) -> list[dto.Paragraph]:
-        ...
-    def queryParagraphPuzzles(self, query: Logic) -> list[dto.ParagraphPuzzle]:
-        ...
-    def querySections(self, query: Logic) -> list[dto.Section]:
-        ...
-    def queryCardPuzzles(self, query: Logic) -> list[dto.CardPuzzle]:
-        ...
-    def queryCardSchedules(self, query: Logic) -> list[dto.CardSchedule]:
-        ...
-    def queryTokenSchedules(self, query: Logic) -> list[dto.TokenSchedule]:
-        ...
-    def queryTokenRequirements(self, query: Logic) -> list[dto.TokenRequirement]:
-        ...
 
+_repositories: dict[str, Repository] = {}
+_default: str = ""
 
-_repositories = {}
-_default = None
-
-def query_(table, logic, populates):
-    ...
+def query(table: type, logic: dto.Logic, populates: dto.DTO = None, repository: str = _default) -> list[dto.DTO]:
+    if repository not in _repositories:
+        raise KeyError("Repository of the key \"{repository}\" was not found")
+    return _repositories[repository].query(table, logic, populates)
 
 def register(registry: Repository, name: str):
     if not len(_repositories.keys()):
