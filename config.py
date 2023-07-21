3a551629ff650e706b89d8ef5793e107672714c1
Ivan Ryzhkov <32067622+JR000@users.noreply.github.com>
2023-07-21 19:06:23 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# TODO: move this to env-virables or to args of main.py
+
+EXTENSIONS_FOLDER = './extensions'
+EXTENSIONS_JSON = './extensions/extensions.json'
+
+"""
+basic config:
+    - path to extensions
+        - directory
+        - json file
+"""
+
+
